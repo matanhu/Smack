721e13f781fe0bad8dcf63d8fba7671361b341aa
MatanHu <MatanHuja@gmail.com>
2018-09-11 02:22:10 +0300

    Design Login Page

---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by MatanHuja on 11/09/2018.
+//  Copyright © 2018 Matan Huja. All rights reserved.
+//
+
+import Foundation
+let TO_LOGIN = "toLogin"
