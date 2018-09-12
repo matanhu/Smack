bee9eb9421b38a88f4d21038ff8b95999ecc2084
MatanHu <MatanHuja@gmail.com>
2018-09-12 20:56:13 +0300

    Start implement Avatar picker view

---
+++ Smack/View/AvatarCell.swift
@@ -0,0 +1,26 @@
+//
+//  AvatarCell.swift
+//  Smack
+//
+//  Created by MatanHuja on 12/09/2018.
+//  Copyright © 2018 Matan Huja. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+    }
+    
+    func setUpView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+    
+}
