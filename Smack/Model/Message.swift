36cf1e99783ac5a963fb5a46f2e68982c5045489
MatanHu <MatanHuja@gmail.com>
2018-09-20 00:40:46 +0300

    Fetching Messages

---
+++ Smack/Model/Message.swift
@@ -0,0 +1,19 @@
+//
+//  Message.swift
+//  Smack
+//
+//  Created by MatanHuja on 20/09/2018.
+//  Copyright © 2018 Matan Huja. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    public private(set) var message: String!
+    public private(set) var userName: String!
+    public private(set) var channeId: String!
+    public private(set) var userAvatar: String
+    public private(set) var userAvatarColor: String
+    public private(set) var id: String
+    public private(set) var timeStamp: String
+}
