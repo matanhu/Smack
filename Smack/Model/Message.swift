674cbc6d1459e86a24d71b069c5a07a1d8eae469
MatanHu <MatanHuja@gmail.com>
2018-09-20 10:48:07 +0300

    Display chat message timeStamp

--- Smack/Model/Message.swift
+++ Smack/Model/Message.swift
@@ -12,8 +12,8 @@ struct Message {
     public private(set) var message: String!
     public private(set) var userName: String!
     public private(set) var channeId: String!
-    public private(set) var userAvatar: String
-    public private(set) var userAvatarColor: String
-    public private(set) var id: String
-    public private(set) var timeStamp: String
+    public private(set) var userAvatar: String!
+    public private(set) var userAvatarColor: String!
+    public private(set) var id: String!
+    public private(set) var timeStamp: String!
 }
