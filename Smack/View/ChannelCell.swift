884565ebe2a9267ae9d584f81b6fe2c2f8fc5e39
MatanHu <MatanHuja@gmail.com>
2018-09-20 10:26:58 +0300

    Implement UI for Unread Channels

--- Smack/View/ChannelCell.swift
+++ Smack/View/ChannelCell.swift
@@ -31,6 +31,13 @@ class ChannelCell: UITableViewCell {
     func configureCell(channel: Channel) {
         let title = channel.name ?? ""
         channelName.text = "#\(title)"
+        channelName.font = UIFont(name: "HelveticaNeue-Regular", size: 17)
+        
+        for id in MessageService.instance.unreadChannels {
+            if id == channel._id {
+                channelName.font = UIFont(name: "HelveticaNeue-Bold", size: 22)
+            }
+        }
     }
     
 
